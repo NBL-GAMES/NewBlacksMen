4c10a9eb9349f2f5f1ace379bf8df13a944b46f3
Jubureba <eng.thiagolima@hotmail.com>
2019-03-13 17:49:07 -0300

    add script move
    add script move

--- Assets/Scripts/Person_Move.cs
+++ Assets/Scripts/Person_Move.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Person_Move : MonoBehaviour {
 
+
+    private Rigidbody2D rb;
+    public float speedMove;
 	// Use this for initialization
 	void Start () {
-		
+        this.rb = GetComponent<Rigidbody2D>();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speedMove);
+        /*
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("WALK TO RIGHT");
+        }else if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("WALK TO LEFT");
+        }*/
 	}
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+
+	}
+
+    void FixedUpdate()
+    {
+        //move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+        
+    }
+}
